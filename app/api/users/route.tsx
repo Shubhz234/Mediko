1ac7b71e6e8cca24e18433b616f82856bff30ff7
Prashant <prashan1837d@gmail.com>
2025-09-04 09:13:34 +0530

    feat: add MedicalVoiceAgent component and Provider for user management; implement user creation logic

---
+++ app/api/users/route.tsx
@@ -0,0 +1,30 @@
+import { db } from "@/config/db";
+import { usersTable } from "@/config/schema";
+import { currentUser } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+export async function POST(req: NextResponse) {
+    const user = await currentUser();
+
+    try {
+        const users = await db.select().from(usersTable)
+            //@ts-ignore
+            .where(eq(usersTable.email, user?.primaryEmailAddress?.emailAddress))
+        if (users?.length == 0) {
+            const result = await db.insert(usersTable).values({
+                //@ts-ignore
+                name: user?.fullName,
+                email: user?.primaryEmailAddress?.emailAddress,
+                credits: 10
+                //@ts-ignore
+            }).returning({ usersTable })
+        return NextResponse.json(result)
+
+        }
+
+        return NextResponse.json(users[0])
+    } catch (e) {
+        return NextResponse.json(e);
+    }
+}
