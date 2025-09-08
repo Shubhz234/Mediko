013b5514bcf76ad5d770ca6799ea20c8913399c4
Prashant <prashan1837d@gmail.com>
2025-09-09 01:44:32 +0530

    feat: implement history list fetching and display in HistoryList component

--- app/(routes)/dashboard/_components/HistoryList.tsx
+++ app/(routes)/dashboard/_components/HistoryList.tsx
@@ -1,19 +1,31 @@
 'use client'
 
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AddNewSessionDialog from './AddNewSessionDialog'
+import axios from 'axios'
 
 const HistoryList = () => {
     const [HistoryList, setHistoryList] = useState([])
+
+    useEffect(() => {
+        GetHistoryList()
+     }, [])
+
+    const GetHistoryList = async () => {
+        const result = await axios.get('/api/session-chat?sessionId=all');
+        console.log(result.data);
+        setHistoryList(result.data)
+    }
+
     return (
         <div className='mt-10'>
             {HistoryList.length == 0 ?
                 <div className='flex items-center justify-between flex-col p-7 border-dashed rounded-2xl border-2'>
-                    <Image src={'/medical-assistance.png'} alt='mediko' width={150} height={150}/>
+                    <Image src={'/medical-assistance.png'} alt='mediko' width={150} height={150} />
                     <h2 className='font-bold text-xl'>No Recent Consultantions</h2>
                     <p>Its looks likke you haven't consult any doctors yet.</p>
-                    <AddNewSessionDialog/>
+                    <AddNewSessionDialog />
                 </div>
                 : <div>List</div>}
         </div>
--- app/api/session-chat/route.tsx
+++ app/api/session-chat/route.tsx
@@ -1,7 +1,7 @@
 import { db } from "@/config/db";
 import { SessionChatTable } from "@/config/schema";
 import { currentUser } from "@clerk/nextjs/server";
-import { eq } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 import { v4 as uuidv4 } from 'uuid'
 
@@ -31,8 +31,18 @@ export async function GET(req: NextRequest) {
     const sessionId = searchParams.get('sessionId');
     const user = await currentUser();
 
-    const result = await db.select().from(SessionChatTable)
-        .where(eq(SessionChatTable.sessionId, sessionId!));
+    if (sessionId === 'all') {
 
-    return NextResponse.json(result[0]);
+        const result = await db.select().from(SessionChatTable)
+            //@ts-ignore
+            .where(eq(SessionChatTable.createdBy, user?.primaryEmailAddress?.emailAddress))
+            .orderBy(desc(SessionChatTable.id));
+        return NextResponse.json(result);
+
+    } else {
+        const result = await db.select().from(SessionChatTable)
+            //@ts-ignore
+            .where(eq(SessionChatTable.sessionId, sessionId));
+        return NextResponse.json(result[0]);
+    }
 }
